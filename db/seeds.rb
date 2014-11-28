25c0e8c5e09bbc4637d09d1b497ad0c21f7d3aab
jmperez <jmperez.85@gmail.com>
2014-11-28 09:49:35 -0300

    Add seeds and other stuff.

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+
+Mathmath::SpecialNumber.create name:"e", value: 2.71828, description: <<-EOS
+  The number e is an important mathematical constant that is the base of the natural logarithm. It is approximately equal to 2.71828,[1] and is the limit of (1 + 1/n)n as n approaches infinity, an expression that arises in the study of compound interest. It can also be calculated as the sum of the infinite series[2]
+  EOS
